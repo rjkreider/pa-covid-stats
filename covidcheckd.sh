505db1efb81b2419b30b0803ff3b0d37076f537e
Rich Kreider <rjkreider@gmail.com>
2020-03-27 12:19:17 -0400

    starting to add some notification stuff.  PA DOH changed website and is actually giving more data now but I had to alter the table scrape to accommodate.

---
+++ covidcheckd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+updatefile=lastupdated.txt
+python3 pa-covid-stats.py
--- pa-covid-stats.py
+++ pa-covid-stats.py
@@ -4,13 +4,14 @@ import requests
 
 url = r'https://www.health.pa.gov/topics/disease/coronavirus/Pages/Cases.aspx'
 html_content = requests.get(url).text
-
+lastupdatedfile = "lastupdated.txt"
 soup = BeautifulSoup(html_content, "lxml")
 stats = soup.find("span",attrs={"class": "ms-rteStyle-Quote"})
 lastupdated = stats.text[stats.text.find('last'):]
 
+
 tables = pd.read_html(html_content, header=0)
-df = tables[1]
+df = tables[3]
 
 totalCounties = 67
 
@@ -33,4 +34,10 @@ print("Counties Reporting Cases: {}".format(reportingCases.count()))
 print("Counties Reporting Cases(%): {}".format(reportingCasesPct))
 print("Counties Reporting Deaths: {}".format(reportingDeaths))
 print("Counties Reporting Deaths(% of counties reporting cases): {}".format(reportingDeathsPct))
+
+if lastupdated != open(lastupdatedfile).read():
+	f=open(lastupdatedfile,"w")
+	f.write(lastupdated)
+	f.close()
+	# Add some notification stuff here...
 
