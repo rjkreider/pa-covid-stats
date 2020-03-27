8f8cf04469ef2095a61ffaefbb003abc321f2a74
rjkreider <46856091+rjkreider@users.noreply.github.com>
2020-03-27 11:10:11 -0400

    Create pa-covid-stats.py

---
+++ pa-covid-stats.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+
+url = r'https://www.health.pa.gov/topics/disease/coronavirus/Pages/Cases.aspx'
+html_content = requests.get(url).text
+
+soup = BeautifulSoup(html_content, "lxml")
+stats = soup.find("span",attrs={"class": "ms-rteStyle-Quote"})
+lastupdated = stats.text[stats.text.find('last'):]
+
+tables = pd.read_html(html_content, header=0)
+df = tables[1]
+
+print("Pennsylvania Data ({})".format(lastupdated))
+# Counties with cases
+#print(df[df.Cases > 0])
+
+deathsTotal = int(df["Deaths"].sum())
+casesTotal  = int(df["Cases"].sum())
+mortalityPercent = round((deathsTotal / casesTotal) * 100,2)
+reportingTotal = int(df["County"].count())
+reportingCases = df["Cases"] >50
+reportingCasesPct = round((reportingCases.count() / reportingTotal) * 100,2)
+reportingDeathsObj = df.apply(lambda x: True if x['Deaths'] > 0 else False, axis=1)
+reportingDeaths = len(reportingDeathsObj[reportingDeathsObj == True].index)
+reportingDeathsPct = round((reportingDeaths / reportingTotal) * 100,2)
+
+print("Cases: {}\nDeaths: {}\nMortality(%): {}\nCounties Reporting: {}\nCounties Positive: {}\nCounties Positive Affected(%): {}\nCounties Deaths: {}\nCounties Deaths(%): {}".format(casesTotal,deathsTotal,mortalityPercent,reportingTotal,reportingCases.count(),reportingCasesPct,reportingDeaths,reportingDeathsPct))
